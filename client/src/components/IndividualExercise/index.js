a8e289a7a43e1c3e16d602a89b75a2f03addf025
Aaron Lisker <aelisker@gmail.com>
2020-08-20 15:01:54 -0700

    adding current before modification

--- client/src/components/IndividualExercise/index.js
+++ client/src/components/IndividualExercise/index.js
@@ -12,17 +12,14 @@ function IndividualExercise(exercise) {
     workoutCategory,
     imageName
   } = exercise;
-  console.log(imageName);
 
   return (
 
     <div className="col-6 mb-3 align-items-stretch">
-      {/* <img src= {require(`../../assets/img/${imageName}`)}/> */}
       <Card>
-        {/* <Card.Img variant="top" src="holder.js/100px160" /> */}
-        <Card.Img variant="top" src={require(`../../assets/img/${imageName}`)}/>
         <Card.Body>
           <Link to={`/exercise/${_id}`}>
+            <Card.Img variant="top" src={require(`../../assets/img/${imageName}`)}/>
             <Card.Title>{name}</Card.Title>
           </Link>
           <Card.Text>
@@ -34,16 +31,6 @@ function IndividualExercise(exercise) {
         </Card.Footer> */}
       </Card>
     </div>
-    // <div className="card px-1 py-1">
-    //   <Link to={`/exercise/${_id}`}>
-    //     {/* <img
-    //       alt={name}
-    //       src={`/images/${image}`}
-    //     /> */}
-    //     <p>{name}</p>
-    //     <p>{description}</p>
-    //   </Link>
-    // </div>
   );
 }
 
--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,91 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/react-hooks';
 
 import { QUERY_EXERCISE } from '../utils/queries';
-import { useMutation } from '@apollo/react-hooks';
-import { SAVE_ROUTINE } from '../utils/mutations';
-import YouTube from 'react-youtube';
-
 import spinner from '../assets/spinner.gif'
 
 function Detail() {
   const { id } = useParams();
-  const [errorMessage, setErrorMessage] = useState('');
-  const [currentExercise, setCurrentExercise] = useState({});
   const { loading, data } = useQuery(QUERY_EXERCISE, {
     variables: { _id: id }
   });
   const exercise = data?.exercise || [];
 
-  useEffect(() => {
-    setCurrentExercise(exercise, exercise.workoutCategory);
-  }, [exercise, id]);
-
-  const [saveRoutine, { error }] = useMutation(SAVE_ROUTINE);
-
-  console.log('currentExercise', currentExercise)
-  console.log('currentExercise.workoutCategory', currentExercise.workoutCategory);
-
-  const saveTheRoutine = async event => {
-    // event.preventDefault();
-    try {
-      const submit = await saveRoutine({
-        variables: {
-          workoutId: formState.workoutId, input: [
-            {
-              name: currentExercise.name, description: currentExercise.description, videoLink: currentExercise.videoLink,
-              trackReps: currentExercise.trackReps, trackWeight: currentExercise.trackWeight,
-              trackDistance: currentExercise.trackDistance, trackTime: currentExercise.trackTime,
-              reps: Number(formState.reps), weight: Number(formState.weight),
-              distance: Number(formState.distance), time: Number(formState.time)
-            }]
-        }
-      })
-      setFormState({
-        ...formState,
-        workoutId: submit.data.saveRoutine._id.toString(),
-      });
-      console.log('WORKOUT ID', formState.workoutId);
-
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  const [formState, setFormState] = useState({ time: 0, weight: 0, reps: 0, distance: 0, workoutId: null });
-  const [workouts, setWorkouts] = useState([])
-  // const [count, setCount] = useState(0)
-
-  const formFunction = () => {
-    // setCount(count + 1)
-    setWorkouts([...workouts, { time: formState.time, weight: formState.weight, reps: formState.reps, distance: formState.distance }])
-  }
-
-  function handleSubmit(e) {
-    e.preventDefault();
-  }
-
-  function handleChange(e) {
-      if (e.target.name === 'time' || e.target.name === 'weight' || e.target.name === 'reps' || e.target.name === 'distance' ) {
-        if(e.target.value === '' || e.target.value === "0") {
-          setErrorMessage(e.target.name.toUpperCase() + ' field is required')
-        } else {
-          setErrorMessage('')
-        }
-      }
-      
-      if (!errorMessage) {
-        setFormState({ ...formState, [e.target.name]: e.target.value });
-      }
-    console.log('errorMessage', errorMessage);
-  }
-
   return (
     <>
-      {currentExercise ? (
-        <div>
-          <div className="container my-1">
+      {exercise ? (
+        <div className="container justify-content-center">
+          <div className="justify-content-center my-1">
             <Link 
               to="/list"
               className="mb-3">
@@ -93,20 +24,20 @@ function Detail() {
             </Link>
 
             <h2>
-              {currentExercise.name} - 
+              {exercise.name} - 
               <small className="mb-3">
-              {' '}{currentExercise.workoutCategory ? currentExercise.workoutCategory.name : null}
+              {' '}{exercise.workoutCategory ? exercise.workoutCategory.name : null}
             </small>
             </h2>
 
             <p className="mt-3">
-              {currentExercise.description}
+              {exercise.description}
             </p>
 
             <iframe 
               width="560" 
               height="315" 
-              src={`https://www.youtube.com/embed/${currentExercise.videoLink}`} 
+              src={`https://www.youtube.com/embed/${exercise.videoLink}`} 
               frameborder="0" 
               allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
               allowfullscreen>
